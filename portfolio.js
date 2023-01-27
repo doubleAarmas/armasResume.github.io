75f7f806d4f56bcca05c5381ea2c2d882fa304be
Alfredo Armas <116199725+doubleAarmas@users.noreply.github.com>
2023-01-27 11:33:38 -0500

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,21 @@
-//scroll to top functionality
+// Nav hamburgerburger selections
 
-const scrollUp = document.getElementById("#scroll-up");
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
 
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
+burger.addEventListener("click", () => {
+  ul.classList.toggle("show");
+})
 
+// Close hamburger menu when a link is clicked
+
+// Select nav links
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((link)=>
+link.addEventListener("click", ()=>{
+  ul.classList.remove("show");
+})
+);
