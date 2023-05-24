7e7b7882b868de29ed1202749e56d9f2ddac1b8c
Alfredo Armas <116199725+doubleAarmas@users.noreply.github.com>
2023-05-24 16:15:28 -0400

    Add files via upload

--- Assets/script.js
+++ Assets/script.js
@@ -16,6 +16,23 @@ const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach((link)=>
 link.addEventListener("click", () => {
-  ul.classList.toggle("show");
+  ul.classList.remove("show");
 })
 );
+
+
+
+//  // Add event listener to the burger menu button
+//  document.getElementById("burger-menu").addEventListener("click", function () {
+//   // Toggle the 'active' class on the navbar
+//   document.querySelector(".navbar").classList.toggle("active");
+// });
+
+// // Add event listener to each navbar link
+// var navLinks = document.querySelectorAll(".nav_links");
+// for (var i = 0; i < navLinks.length; i++) {
+//   navLinks[i].addEventListener("click", function () {
+//       // Remove the 'active' class from the navbar
+//       document.querySelector(".navbar").classList.remove("active");
+//   });
+// }
